d40c4f5060ca2d8c7a1cb0014fdbf82effe35f3b
Sadnan-2019 <sadnan.wow@gmail.com>
2022-11-23 17:04:48 +0600

    nav and slider

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Feture from '../Feture/Feture';
+import Slider from '../Slider/Slider';
+
+const Home = () => {
+    return (
+        <div>
+            
+            <main id="main">
+            <Slider></Slider>
+                <Feture></Feture>
+                </main>
+            
+        </div>
+    );
+};
+
+export default Home;
